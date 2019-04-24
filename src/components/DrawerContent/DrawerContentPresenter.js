8b733538c50232dc3764f7e8f5a41460faadf6b9
김성훈 <seongh7800@douzone.com>
2019-04-24 17:49:33 +0900

    인하우스 0.1.3

--- src/components/DrawerContent/DrawerContentPresenter.js
+++ src/components/DrawerContent/DrawerContentPresenter.js
@@ -12,18 +12,50 @@ import ProfileImage from '../../../assets/icons/imgVcNophoto_2x.png';
 const DrawerContentPresenter = props => {
   return (
     <View style={styles.container}>
-      <View style={{ flexDirection: 'row', marginTop: 6, padding: 10 }}>
+      <View
+        style={{
+          // flexDirection: 'row',
+          justifyContent: 'center',
+          paddingTop: 25,
+          paddingBottom: 10,
+          alignItems: 'center',
+          borderBottomWidth: 1,
+          borderColor: '#cccccc'
+        }}
+      >
         <View style={styles.profile}>
           <Image source={ProfileImage} style={styles.profile_img} />
         </View>
         {props.auth.user_name && (
-          <View style={{ padding: 16 }}>
-            <Text>
-              {props.auth.user_name} ({props.auth.portal_id})
+          <View
+            style={
+              {
+                // padding: 17,
+                // paddingTop: 27,
+                // paddingBottom: 27
+              }
+            }
+          >
+            <Text
+              style={{
+                textAlign: 'center',
+                marginTop: 10,
+                fontSize: 17.5,
+                color: '#515151',
+                fontWeight: 'bold'
+              }}
+            >
+              {props.auth.user_name}{' '}
+              {/* <Text style={{ fontSize: 13 }}>({props.auth.portal_id})</Text> */}
             </Text>
+            <Text
+              style={{ textAlign: 'center', fontSize: 13, color: '#6d6d6d' }}
+            >
+              {props.auth.last_company.company_name_kr}
+            </Text>
+            <Text style={{}} />
             {/* <Text>{props.auth.portal_id}</Text> */}
             {/* <Text>{props.auth.last_company.full_path}</Text> */}
-            <Text>{props.auth.last_company.company_name_kr}</Text>
             {/* <Text>{JSON.stringify(props.auth)}</Text> */}
             {/* <Text>{props.auth.profile_url}</Text> */}
           </View>
@@ -54,15 +86,15 @@ const styles = StyleSheet.create({
     backgroundColor: 'transparent'
   },
   profile: {
-    width: 50,
-    height: 50,
+    width: 80,
+    height: 80,
     alignSelf: 'center',
     borderRadius: 50,
     overflow: 'hidden'
   },
   profile_img: {
-    width: 50,
-    height: 50,
+    width: 80,
+    height: 80,
     resizeMode: 'contain',
     backgroundColor: '#00000030'
   },
