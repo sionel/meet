a24558eb3ad65bad2c0a8620172415959c06fd9b
peacejung <peacejung@douzone.com>
2019-09-03 13:37:04 +0900

    화상대화 속도개선

--- src/utils/conference/config/wehagov.js
+++ src/utils/conference/config/wehagov.js
@@ -2,28 +2,29 @@
  * wehagov 기본 config
  */
 const config = {
-	hosts: {
-		domain: 'video.wehagov.com',
-		muc: 'conference.video.wehagov.com'
-	},
+  hosts: {
+    domain: 'video.wehagov.com',
+    muc: 'conference.video.wehagov.com'
+  },
 
-	bosh: '//video.wehagov.com/http-bind',
-	openBridgeChannel: 'datachannel',
-	channelLastN: -1,
-	resolution: '720',
-	constraints: {
-		video: {
-			aspectRatio: 1.3,
-			height: { ideal: 720, min: 720 },
-			width: { min: 1280, max: 1280 }
-		}
-	},
-	minHDHeight: 720,
-	p2p: {
-		enabled: false
-	},
-	stereo: true,
-	e2eping: { pingInterval: 10000, analyticsInterval: 60000 },
-	disableSimulcast: true
+  bosh: '//video.wehagov.com/http-bind',
+  openBridgeChannel: 'datachannel',
+  channelLastN: -1,
+  resolution: 720,
+  constraints: {
+    video: {
+      aspectRatio: 1.3,
+      height: { ideal: 720, min: 240, max: 720 },
+      width: { min: 640, max: 1280 }
+    }
+  },
+  disableSuspendVideo: true,
+  minHDHeight: 240,
+  p2p: {
+    enabled: false
+  },
+  stereo: true,
+  e2eping: { pingInterval: 10000, analyticsInterval: 60000 },
+  disableSimulcast: true
 };
 export default config;
