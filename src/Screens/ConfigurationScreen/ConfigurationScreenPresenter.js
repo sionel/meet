2e844c1abff5cd5e18c84204a0238383d43418de
peacejung <peacejung@douzone.com>
2020-02-04 17:24:22 +0900

    1.3.0

--- src/Screens/ConfigurationScreen/ConfigurationScreenPresenter.js
+++ src/Screens/ConfigurationScreen/ConfigurationScreenPresenter.js
@@ -47,7 +47,7 @@ const ConfigurationScreenPresenter = props => {
     {
       title: '버전정보',
       icon: 'ico_menu_info',
-      content: Platform.OS === 'ios' ? '1.24.10' : '1.2.1'
+      content: Platform.OS === 'ios' ? '1.24.10' : '1.3.0'
     },
     {
       title: '이용약관 및 법률정보',
