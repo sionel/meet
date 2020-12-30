9263d09dfe1901fec3984a66f9469d59eff2c3fd
sadb0101 <sadb0101@naver.com>
2020-12-30 17:14:10 +0900

    외부참여자 라벨 붙임

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ console.reportErrorsAsExceptions = false;
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { loaded: false, url: 'com.wehago.meet://?login_info=mobile&type=conference&mPORTAL_ID=seongh7800&mHASH_KEY=345252464567578596789078&mAuth_r_token=Gj2EWgBeA3tpu4zEu8PdzIynaha&mAuth_a_token=Gj2EWgBeA3tpu4zEu8PdzIynaha&cno=9&video_id=31278c02-410e-4420-8deb-ae905f41aebb' };
+    this.state = { loaded: false, url: props.url.url };
     // this.state = { loaded: false, url: 'wehago.meet://?login_info=email&type=conference&token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJ2aWRlby53ZWhhZ28uY29tIiwicm9vbSI6IjkxZGY1ZmFlLTQzNTAtNDdiMC1iMTZjLTdmYzk2MzRmMTg1ZCIsImVtYWlsIjoic2FkYjAxMDFAbmF2ZXIuY29tIiwiaWF0IjoxNjA0OTA2MjgxLCJleHAiOjE5MjAyNjYyODF9.lY7h6sYaKfrWBQkGa1pZhSF9auhsRsMuzqQtsBi8evQ' };
   }
 
