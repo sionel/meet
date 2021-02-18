3ce7d37109afb802a73f04fd542bf3cc2562bf3a
sadb0101 <sadb0101@naver.com>
2021-02-18 16:38:33 +0900

    운영기 및 주석 제거

--- src/utils/conference/config/dev.js
+++ src/utils/conference/config/dev.js
@@ -5,38 +5,6 @@ const isWehagoV = WEHAGO_ENV === 'WEHAGOV';
 /**
  * wehago 기본 config
  */
-// const config = {
-//   hosts: {
-//     domain: "video.wehago.com",
-//     muc: "conference.video.wehago.com",
-//   },
-
-//   bosh: "//video.wehago.com/http-bind",
-//   openBridgeChannel: "datachannel",
-//   channelLastN: -1,
-//   resolution: 720,
-//   constraints: {
-//     video: {
-//       aspectRatio: 1.3,
-//       height: {
-//         ideal: 720,
-//         max: 720,
-//         min: 240,
-//       },
-//       width: { min: 640, max: 1280 },
-//     },
-//   },
-//   disableSuspendVideo: true,
-//   disableSimulcast: false,
-//   minHDHeight: 240,
-//   p2p: {
-//     enabled: true,
-//   },
-//   stereo: true,
-//   e2eping: { pingInterval: 10000, analyticsInterval: 60000 },
-//   devMode: true,
-//   useIPv6: true
-// };
 
 //  개발기용
 const config = {
