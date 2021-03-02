b8f977379f69e22ca9cd821cf9e5ae81c035ebdf
sadb0101 <sadb0101@naver.com>
2021-03-02 15:49:49 +0900

    util 추가

--- jitsi/features/base/util/index.js
+++ jitsi/features/base/util/index.js
@@ -1,6 +1,6 @@
 export * from './helpers';
 export * from './httpUtils';
 export * from './loadScript';
-export * from './openURLInBrowser';
+// export * from './openURLInBrowser';
 export * from './uri';
 export * from './parseURLParams';
--- jitsi/features/base/util/openURLInBrowser.native.js
+++ jitsi/features/base/util/openURLInBrowser.native.js
@@ -1,17 +1,17 @@
-// @flow
+// // @flow
 
-import { Linking } from 'react-native';
+// import { Linking } from 'react-native';
 
-import logger from './logger';
+// import logger from './logger';
 
-/**
- * Opens URL in the browser.
- *
- * @param {string} url - The URL to be opened.
- * @returns {void}
- */
-export function openURLInBrowser(url: string) {
-    Linking.openURL(url).catch(error => {
-        logger.error(`An error occurred while trying to open ${url}`, error);
-    });
-}
+// /**
+//  * Opens URL in the browser.
+//  *
+//  * @param {string} url - The URL to be opened.
+//  * @returns {void}
+//  */
+// export function openURLInBrowser(url: string) {
+//     Linking.openURL(url).catch(error => {
+//         logger.error(`An error occurred while trying to open ${url}`, error);
+//     });
+// }
