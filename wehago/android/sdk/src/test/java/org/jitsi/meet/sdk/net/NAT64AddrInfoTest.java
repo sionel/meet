23879a9861a1238bae3afc17cd1d01e1143fd6c9
sadb0101 <sadb0101@naver.com>
2021-08-05 14:13:00 +0900

    android 폴더 복사

--- wehago/android/sdk/src/test/java/org/jitsi/meet/sdk/net/NAT64AddrInfoTest.java
+++ wehago/android/sdk/src/test/java/org/jitsi/meet/sdk/net/NAT64AddrInfoTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.wehago.meet.sdk.net;
+package org.jitsi.meet.sdk.net;
 
 import org.junit.Test;
 
