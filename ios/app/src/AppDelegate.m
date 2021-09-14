717880a07e13078795e53e307d58540ac5ce8079
sadb0101 <sadb0101@naver.com>
2021-09-14 10:56:19 +0900

    ios 설정

--- ios/app/src/AppDelegate.m
+++ ios/app/src/AppDelegate.m
@@ -34,7 +34,7 @@
     jitsiMeet.defaultConferenceOptions = [JitsiMeetConferenceOptions fromBuilder:^(JitsiMeetConferenceOptionsBuilder *builder) {
         [builder setFeatureFlag:@"resolution" withValue:@(360)];
         [builder setFeatureFlag:@"ios.screensharing.enabled" withBoolean:YES];
-        builder.serverURL = [NSURL URLWithString:@"https://meet.jit.si"];
+        // builder.serverURL = [NSURL URLWithString:@"https://meet.jit.si"];
         builder.welcomePageEnabled = YES;
 
         // Apple rejected our app because they claim requiring a
